9dbf6265693c12d8ca1c860057ad7c3125b2ea16
TangYanYee <72559487+TangYanYee@users.noreply.github.com>
2025-03-20 17:21:24 +0800

    Create Digital_Analog.ino

---
+++ examples/Version4/Digital_Analog/Digital_Analog.ino
@@ -0,0 +1,63 @@
+#include <PeanutKingSoccerV4.h>
+static PeanutKingSoccerV4 robot = PeanutKingSoccerV4();
+
+void setup() {
+  robot.init(); 
+  pinMode(S1_P, OUTPUT);
+  pinMode(S2_P, OUTPUT);
+  pinMode(S3_P, OUTPUT);
+  pinMode(S4_P, OUTPUT);
+
+  pinMode(D1_P, INPUT_PULLUP);
+  pinMode(D2_P, INPUT_PULLUP);
+  pinMode(D3_P, INPUT_PULLUP);
+  pinMode(D4_P, OUTPUT);
+  pinMode(D5_P, OUTPUT);
+  pinMode(D6_P, OUTPUT);
+
+  pinMode(A1_P, INPUT);
+  pinMode(A2_P, INPUT);
+  pinMode(A3_P, INPUT);
+  pinMode(A4_P, INPUT);
+}
+
+void loop() {
+  analogWrite(S1_P, 60); 
+  analogWrite(S2_P, 70); 
+  analogWrite(S3_P, 100);
+  analogWrite(S4_P, 200);
+
+  digitalWrite(D4_P, HIGH);
+  digitalWrite(D5_P, HIGH);
+  digitalWrite(D6_P, HIGH);
+
+
+  Serial.println("DigitalRead:");
+  Serial.print(digitalRead(D1_P));
+  Serial.print(" ");
+  Serial.print(digitalRead(D2_P));
+  Serial.print(" ");
+  Serial.print(digitalRead(D3_P));
+  Serial.print("\n");
+
+  Serial.println("AnalogRead:");
+  Serial.print(analogRead(A1_P));
+  Serial.print(" ");
+  Serial.print(analogRead(A2_P));
+  Serial.print(" ");
+  Serial.print(analogRead(A3_P));
+  Serial.print(" ");
+  Serial.print(analogRead(A4_P));
+  Serial.print("\n");
+
+  delay(200);
+  digitalWrite(D4_P, LOW);
+  digitalWrite(D5_P, LOW);
+  digitalWrite(D6_P, LOW);
+  
+
+  delay(200);
+
+}
+
+  
