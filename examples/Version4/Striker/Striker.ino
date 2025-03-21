ac2108596b5e599375bc5889792c1761911ff2dd
TangYanYee <72559487+TangYanYee@users.noreply.github.com>
2025-03-21 16:39:08 +0800

    dk why example from v3 is wrong

--- examples/Version4/Striker/Striker.ino
+++ examples/Version4/Striker/Striker.ino
@@ -18,16 +18,16 @@ void loop() {
   maxEye = robot.compoundMaxEye();             // Maximum Eye         - The infrared senor with highest reading
   maxEyeReading = robot.compoundMaxEyeVal();      // Maximum Eye Reading - The reading from the Maximum Eye
   
-  if( maxEye == 1 ){
+  if( maxEye == 0|| maxEye == 1 || maxEye == 2){
     motor(90,90,-90,-90);
   }
   else if( maxEye == 3 || maxEye == 4 || maxEye == 5){
-    motor(-90,0,90,-0);
+    motor(-90,90,90,-90);
   }
   else if( maxEye == 6 || maxEye == 7 || maxEye == 8){
-    motor(-90,0,90,-0);
+    motor(90,-90,-90,90);
   }
   else if( maxEye == 9 || maxEye == 10 || maxEye ==11){
-    motor(0,-90,0,90);
+    motor(-90,-90,90,90);
   }
 }
